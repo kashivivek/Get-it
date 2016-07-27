3e480ce4f7718de177d0d54e7dbadd61fc0b9075
kashivivek <kashivivek@gmail.com>
2016-07-27 00:04:30 -0700

    design edits

--- app/src/main/java/com/example/vivek/yes/HomeActivity.java
+++ app/src/main/java/com/example/vivek/yes/HomeActivity.java
@@ -10,23 +10,22 @@ import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.MainThread;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.View;
 
 import com.cocosw.bottomsheet.BottomSheet;
 import com.example.vivek.pojo.LatLngBean;
-import com.google.android.gms.analytics.ecommerce.Product;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -37,27 +36,34 @@ import java.util.HashMap;
 public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
     static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
     final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
-    GoogleMap map ;
-    private ArrayList<LatLng>listLatLng = null;
-    ArrayList <LatLng>arrayList = null ;
-    HashMap<Marker,LatLngBean> hashMapMarker = new HashMap<Marker,LatLngBean>();
-    Context ctx ;
+    GoogleMap map;
+    private ArrayList<LatLng> listLatLng = null;
+    ArrayList<LatLng> arrayList = null;
+    HashMap<Marker, LatLngBean> hashMapMarker = new HashMap<Marker, LatLngBean>();
+    Context ctx;
     TestAdapter t;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-       statusCheck();
+        statusCheck();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
     }
-    public void statusCheck()
-    {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home_maps, menu);
+        return true;
+    }
+
+    public void statusCheck() {
         final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             buildAlertMessageNoGps();
         }
     }
@@ -67,7 +73,7 @@ public class HomeActivity extends FragmentActivity implements OnMapReadyCallback
         builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
                 .setCancelable(false)
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog,  final int id) {
+                    public void onClick(final DialogInterface dialog, final int id) {
                         startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
                     }
                 })
@@ -84,10 +90,9 @@ public class HomeActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap map) {
         // Add a marker in Sydney, Australia, and move the camera.
-        try{
+        try {
             map.setMyLocationEnabled(true);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             statusCheck();
         }
 
@@ -118,165 +123,97 @@ public class HomeActivity extends FragmentActivity implements OnMapReadyCallback
                     .tilt(10)                   // Sets the tilt of the camera to 30 degrees
                     .build();                   // Creates a CameraPosition from the builder
             map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));*/
-        ctx=this.getApplicationContext();
+        ctx = this.getApplicationContext();
         t = new TestAdapter(ctx);
         Bundle extras = getIntent().getExtras();
-        String tag=null;
+        String tag = null;
         System.out.println("hi this is tag extra");
         if (extras != null) {
-             tag = extras.getString("tag2");
+            tag = extras.getString("tag2");
             //The key argument here must match that used in the other activity
             System.out.println(tag);
             try {
 
-                LoadingGoogleMap(t.getCordinates(tag),map);
+                LoadingGoogleMap(t.getCordinates(tag), map);
                 System.out.println("cordinates are done");
-            }
-            catch (NullPointerException e){
+            } catch (NullPointerException e) {
                 e.printStackTrace();
             }
         }
 
-
-        // very important
-        /*String jam=null;
-
-
-        String[] parts = jam.split(":");//houshold:electrician
-        double lon =Double.parseDouble(parts[0]);
-        double lat =Double.parseDouble(parts[1]);
-        String id=
-        LatLng xyz = new LatLng(lat,lon);*/
-       // System.out.print("@$#%^!*#^$%&@*!(#^$%@*");
-
-        BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.u);
-       /* LatLng india_home = new LatLng(17.421347, 78.492047);
-        LatLng andra_bank = new LatLng(17.424506, 78.490533);
-        LatLng school = new LatLng(17.423768, 78.492239);
-        LatLng po = new LatLng(17.419704, 78.495178);
-        LatLng temple = new LatLng(17.419787, 78.491166);
-
-
-
-        map.addMarker(new MarkerOptions().position(india_home).title("india_home").icon(icon));
-        map.addMarker(new MarkerOptions().position(school).title("school").icon(icon));
-        map.addMarker(new MarkerOptions().position(andra_bank).title("andra_bank").icon(icon));
-        map.addMarker(new MarkerOptions().position(po).title("po").icon(icon));
-        map.addMarker(new MarkerOptions().position(temple).title("temple").icon(icon));
-
-        map.moveCamera(CameraUpdateFactory.newLatLng(india_home));
-
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(new LatLng(17.421347, 78.492047))      // Sets the center of the map to location user
-                .zoom(16)// Sets the zoom
-                .tilt(10)                   // Sets the tilt of the camera to 30 degrees
-                .build();                   // Creates a CameraPosition from the builder
-        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-
-            @Override
-            public boolean onMarkerClick(Marker arg0) {
-
-                new BottomSheet.Builder(HomeActivity.this).title("fdsn").sheet(R.menu.menu_home).listener(new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-
-                            case R.id.book_service:
-                                Intent intent1 = new Intent(HomeActivity.this, ProductActivity.class);
-                                startActivity(intent1);
-                                *//*Toast.makeText(HomeActivity.this, "Sagar demo", Toast.LENGTH_SHORT).show();*//*
-                                break;
-
-                            case R.id.Profile:
-                                Intent intent = new Intent(HomeActivity.this, ProductActivity.class);
-                                startActivity(intent);
-                                *//*Toast.makeText(HomeActivity.this, "Sagar demo", Toast.LENGTH_SHORT).show();*//*
-                                break;
-                        }
-                    }
-                }).show();
-
-               *//* if (arg0.getTitle().equals("india_home")) { //if marker source is clicked
-                    *//**//*Toast.makeText(HomeActivity.this, arg0.getTitle(), Toast.LENGTH_SHORT).show();// display toast*//**//*
-                    Intent intent = new Intent(HomeActivity.this, TimePass.class);
-                    startActivity(intent);
-                }*//*
-                return true;
-            }
-
-        });*/
-
     }
 
-    void LoadingGoogleMap(ArrayList<LatLngBean> arrayList, GoogleMap googlemap)
-    {
+    void LoadingGoogleMap(ArrayList<LatLngBean> arrayList, GoogleMap googlemap) {
         System.out.println("LoadingGoogle map start");
-        System.out.println("%%%arraylist size is "+arrayList.size());
+        System.out.println("%%%arraylist size is " + arrayList.size());
         LatLngBean bean;
         map = googlemap;
         map.clear();
-            map.getUiSettings().setMyLocationButtonEnabled(true);
-            map.setMyLocationEnabled(true);
-            map.getUiSettings().setZoomControlsEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
 
-            if(arrayList.size()>0)
-            {
-                try
-                {
-                    listLatLng=new ArrayList<>();
-                    for (int i = 0; i < arrayList.size(); i++)
-                    {
-                        bean = arrayList.get(i);
-                        if(bean.getLatitude().length()>0 && bean.getLongitude().length()>0)
-                        {
-                            double lat=Double.parseDouble(bean.getLatitude());
-                            double lon=Double.parseDouble(bean.getLongitude());
-                            System.out.println("%%%% "+lat+","+lon);
-                            LatLng x= new LatLng(lat,lon);
+        if (arrayList.size() > 0) {
+            try {
+                listLatLng = new ArrayList<>();
+                for (int i = 0; i < arrayList.size(); i++) {
+                    bean = arrayList.get(i);
+                    if (bean.getLatitude().length() > 0 && bean.getLongitude().length() > 0) {
+                        double lat = Double.parseDouble(bean.getLatitude());
+                        double lon = Double.parseDouble(bean.getLongitude());
+                        System.out.println("%%%% " + lat + "," + lon);
+                        LatLng x = new LatLng(lat, lon);
 
-                            Marker marker = map.addMarker(new MarkerOptions().title(bean.getName()).position(x).icon(BitmapDescriptorFactory.fromResource(R.drawable.u)));
-                            System.out.println("%%%%%markers are done");
-                            //Add Marker to Hashmap
-                            hashMapMarker.put(marker,bean);
+                        Marker marker = map.addMarker(new MarkerOptions().title(bean.getName()).position(x).icon(BitmapDescriptorFactory.fromResource(R.drawable.u)));
+                        System.out.println("%%%%%markers are done");
+                        //Add Marker to Hashmap
+                        hashMapMarker.put(marker, bean);
 
-                            //Set Zoom Level of Map pin
-                            LatLng object=new LatLng(lat, lon);
-                            listLatLng.add(object);
-                        }
+                        //Set Zoom Level of Map pin
+                        LatLng object = new LatLng(lat, lon);
+                        listLatLng.add(object);
                     }
-                    LatLng zoom = new LatLng(17.3850, 78.4867);
-                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(zoom,12));
-
                 }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
+                LatLng zoom = new LatLng(17.3850, 78.4867);
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(zoom, 12));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
 
-                map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
 
-                    @Override
-                    public void onInfoWindowClick(Marker position)
-                    {
-                        LatLngBean bean=hashMapMarker.get(position);
+                @Override
+                public void onInfoWindowClick(Marker position) {
+                    final LatLngBean bean = hashMapMarker.get(position);
 
-                        new BottomSheet.Builder(HomeActivity.this).title(bean.getName()).sheet(R.menu.menu_home).listener(new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                switch (which) {
+                    new BottomSheet.Builder(HomeActivity.this).title(bean.getName()).sheet(R.menu.menu_home).listener(new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
 
-                                    case R.id.book_service:
-                                        Intent intent1 = new Intent(HomeActivity.this, ProductActivity.class);
-                                        startActivity(intent1);
-                                        /*Toast.makeText(HomeActivity.this, "Sagar demo", Toast.LENGTH_SHORT).show();*/
+                                case R.id.Get_Directions_Service:
+                                    Intent intent2 = new Intent(android.content.Intent.ACTION_VIEW,
+                                            Uri.parse("http://maps.google.com/maps?saddr=0,0&daddr=" + bean.getLatitude() + "," + bean.getLongitude()));
+                                    startActivity(intent2);
+                                    break;
+
+                                case R.id.call:
+                                    String phone = bean.getPhone();
+                                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+                                    startActivity(intent);
                                         break;
 
                                     case R.id.Profile:
-                                        Intent intent = new Intent(HomeActivity.this, ProductActivity.class);
-                                        startActivity(intent);
+                                        Intent intent1 = new Intent(HomeActivity.this, ProductActivity.class);
+                                        intent1.putExtra("BeanName", bean.getName());
+                                        intent1.putExtra("BeanPhone",bean.getPhone());
+                                        intent1.putExtra("BeanLattitude",bean.getLatitude());
+                                        intent1.putExtra("BeanLongitude",bean.getLongitude());
+                                        intent1.putExtra("BeanType",bean.getType());
+                                        intent1.putExtra("BeanImage", bean.getImagepath());
+                                        startActivity(intent1);
                                         /*Toast.makeText(HomeActivity.this, "Sagar demo", Toast.LENGTH_SHORT).show();*/
                                         break;
                                 }
--- app/src/main/java/com/example/vivek/yes/ProductActivity.java
+++ app/src/main/java/com/example/vivek/yes/ProductActivity.java
@@ -1,27 +1,45 @@
 package com.example.vivek.yes;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ProductActivity extends AppCompatActivity {
 
     CollapsingToolbarLayout collapsingToolbarLayout;
     ImageView image;
+    String name, phone, type, lattitude, longitude, imagepath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product2);
         image = (ImageView) findViewById(R.id.image);
-        image.setImageResource(R.drawable.anusha);
+        String url = "https://support.files.wordpress.com/2009/07/pigeony.jpg?w=688";
+        image.setImageBitmap(getBitmapFromURL(url));
+        Bundle extras = getIntent().getExtras();
+        name = extras.getString("BeanName");
+        phone = extras.getString("BeanPhone");
+        lattitude = extras.getString("BeanLattitude");
+        longitude = extras.getString("BeanLongitude");
+        type = extras.getString("BeanType");
+        imagepath = extras.getString("BeanImage");
+
+        System.out.println("name in product activity is "+name);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar1));
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle("Product");
@@ -34,6 +52,21 @@ public class ProductActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // getMenuInflater().inflate(R.menu.sample_actions, menu);
         return true;
+    }
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private void setPalette() {
@@ -49,6 +82,8 @@ public class ProductActivity extends AppCompatActivity {
         });
 
     }
+
+    
 
 
 }
