6de9af0b8e4c935906e5622833424de16a7d23b8
kashivivek <kashivivek@gmail.com>
2016-07-27 14:50:12 -0700

    loading image from url

--- app/src/main/java/com/example/vivek/yes/ProductActivity.java
+++ app/src/main/java/com/example/vivek/yes/ProductActivity.java
@@ -1,50 +1,47 @@
 package com.example.vivek.yes;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
 
 public class ProductActivity extends AppCompatActivity {
 
     CollapsingToolbarLayout collapsingToolbarLayout;
     ImageView image;
-    String name, phone, type, lattitude, longitude, imagepath;
+    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product2);
-        image = (ImageView) findViewById(R.id.image);
-        String url = "https://support.files.wordpress.com/2009/07/pigeony.jpg?w=688";
-        image.setImageBitmap(getBitmapFromURL(url));
-        Bundle extras = getIntent().getExtras();
-        name = extras.getString("BeanName");
-        phone = extras.getString("BeanPhone");
-        lattitude = extras.getString("BeanLattitude");
-        longitude = extras.getString("BeanLongitude");
-        type = extras.getString("BeanType");
-        imagepath = extras.getString("BeanImage");
+        spinner=(ProgressBar)findViewById(R.id.progressBar);
+        spinner.setVisibility(View.VISIBLE);
 
-        System.out.println("name in product activity is "+name);
+        String url = "http://farm1.static.flickr.com/114/298125983_0e4bf66782_b.jpg";
+        //http://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg
+        image = (ImageView) findViewById(R.id.image);
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(ProductActivity.this));
+        imageLoader.displayImage(url, image);
+        spinner.setVisibility(View.GONE);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar1));
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle("Product");
-        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
-        setPalette();
+       // collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.background_dark));
+        collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(android.R.color.background_dark));
+        //setPalette(image);
 
     }
 
@@ -54,22 +51,7 @@ public class ProductActivity extends AppCompatActivity {
         return true;
     }
 
-    public static Bitmap getBitmapFromURL(String src) {
-        try {
-            URL url = new URL(src);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            Bitmap myBitmap = BitmapFactory.decodeStream(input);
-            return myBitmap;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private void setPalette() {
+    private void setPalette(ImageView image) {
         Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
         Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
             @Override
@@ -82,8 +64,5 @@ public class ProductActivity extends AppCompatActivity {
         });
 
     }
-
-    
-
 
 }
